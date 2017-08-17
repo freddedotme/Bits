989af5fa8bb1fd76bf33ec6509e7c24aab5d2f48
freddedotme <r5cx95tw232yvs8w@icloud.com>
2017-08-17 12:35:25 +0200

    Minor changes

--- src/main/java/bits/bits/team/event/EventFishSlap.java
+++ src/main/java/bits/bits/team/event/EventFishSlap.java
@@ -17,39 +17,39 @@ import org.bukkit.util.Vector;
 public class EventFishSlap implements Listener {
   @EventHandler
   public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-	Bukkit.getLogger().info("An entity has damaged another entity");
-  	
-  	if (!(e.getDamager() instanceof Player)) return;
+    Bukkit.getLogger().info("An entity has damaged another entity");
+
+    if (!(e.getDamager() instanceof Player)) return;
     Player damager = (Player) e.getDamager();
 
     Bukkit.getLogger().info("The damager was a player");
-    
+
     if (!(e.getEntity() instanceof Player)) return;
     Player entity = (Player) e.getEntity();
-    
+
     Bukkit.getLogger().info("The damaged entity was a player");
-    
+
     e.setCancelled(true);
-    
+
     ItemStack item = damager.getInventory().getItemInMainHand();
     ItemStack fish = new ItemStack(Material.RAW_FISH, 1);
 
     if (item == null || !item.isSimilar(fish)) return;
-    
+
     Bukkit.getLogger().info("The damager was holding a fish");
-    
+
     double horizontalPower = 2; // length of XZ velocity
     double verticalPower = 0.5; // length of Y velocity
-    
+
     Vector launch = damager.getEyeLocation()
-                           .getDirection()
-                           .setY(0)
-                           .normalize()
-                           .multiply(horizontalPower)
-                           .setY(verticalPower);
-    
+      .getDirection()
+      .setY(0)
+      .normalize()
+      .multiply(horizontalPower)
+      .setY(verticalPower);
+
     entity.setVelocity(entity.getVelocity()
-                             .add(launch));
+      .add(launch));
 
     damager.getInventory().removeItem(fish);
   }
