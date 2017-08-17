57aa51fe328c1a27ed997dda85004e392c461483
Albin Eriksson <albin-eriksson@outlook.com>
2017-08-17 21:40:18 +0200

    Prevent PvP fire damage

--- src/main/java/bits/bits/team/event/EventDisablePvP.java
+++ src/main/java/bits/bits/team/event/EventDisablePvP.java
@@ -1,9 +1,11 @@
 package bits.bits.team.event;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 /**
@@ -14,13 +16,21 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class EventDisablePvP implements Listener {
   @EventHandler
   public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-	if(e.getEntity() instanceof Player)
-    {
-      // If damager is a player, or the damager is a projectile shot by a player
-      if(e.getDamager() instanceof Player
-	    || (e.getDamager() instanceof Projectile
-	      && ((Projectile) e.getDamager()).getShooter() instanceof Player))
-      	e.setCancelled(true);
-    }
+	if(isPvpDamage(e.getDamager(), e.getEntity()))
+	  e.setCancelled(true);
+  }
+  
+  @EventHandler
+  public void onEntityCombustByEntity(EntityCombustByEntityEvent e) {
+	if(isPvpDamage(e.getCombuster(), e.getEntity()))
+	  e.setCancelled(true);
+  }
+  
+  private boolean isPvpDamage(Entity source, Entity target)
+  {
+	// Returns true if the target is a player, and the source is either a player or a projectile shot by a player.
+	return target instanceof Player &&
+	  (source instanceof Player || (source instanceof Projectile
+		&& ((Projectile)source).getShooter() instanceof Player));
   }
 }
